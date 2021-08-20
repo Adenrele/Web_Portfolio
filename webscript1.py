41216c0f4a4059fa25df37cc34d6d36d62d06642
Adenrele <73744685+Adenrele@users.noreply.github.com>
2021-08-20 09:13:50 +0100

    contact page fixed

--- webscript1.py
+++ webscript1.py
@@ -1,6 +1,45 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+from forms import ContactForm
+from flask_mail import Message, Mail
+
+
 
 app = Flask(__name__)
+
+app.secret_key = 'Fucku&DaHoesUCameWit'
+
+app.config["MAIL_DEBUG"] = True
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USE_TLS"] = False
+app.config["MAIL_USERNAME"] = 'adenrele.komolafe@gmail.com'
+app.config["MAIL_PASSWORD"] = 'Adekom1993!'
+ 
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+  form = ContactForm(request.form)
+ 
+  if request.method == 'POST':
+    if form.validate() == False:
+      flash('All fields are required.')
+      return render_template('contact.html', form=form)
+    else:
+      msg = Message(form.subject.data, sender='adenrele.komolafe@gmail.com', recipients=['a.komolafe@outlook.com'])
+      msg.body = """
+      From: %s &lt;%s&gt;
+      %s
+      """ % (form.name.data, form.email.data, form.message.data)
+      
+      mail.send(msg)
+ 
+      return render_template('contact.html', success =True)
+ 
+  elif request.method == 'GET':
+    return render_template('contact.html', form=form)
+
+mail= Mail(app)
+
 
 @app.route('/')
 def home():
@@ -18,10 +57,6 @@ def projects():
 def blogs():
     return render_template("blogs.html")
 
-@app.route('/contact')
-def contact():
-    return render_template("contact.html")
 
 if __name__ == "__main__":
     app.run(debug = True)
-
