1125403e5ae4bc8df47a660e9fcb20cc7155372d
Adenrele <73744685+Adenrele@users.noreply.github.com>
2021-09-12 23:17:07 +0100

    not even sure here now

---
+++ keys.py
@@ -0,0 +1,6 @@
+app_secret_key = "Fucku&DaHoesUCameWitandthensuckyourmotherinsideout229988"
+sendmail =  "adenrele.komolafe@gmail.com"
+sendmailkey = "qagwyd-3vehnA-ranhef"
+
+mailserver = "smtp.gmail.com"
+mailport = 465
