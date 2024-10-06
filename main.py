581dc158da6b43f4a311f53c30c19aa584f0265a
Adenrele <73744685+Adenrele@users.noreply.github.com>
2024-10-06 10:59:28 +0100

    all testing, printing and error checking code removedd

--- main.py
+++ main.py
@@ -45,17 +45,6 @@ def get_access_token():
     response_json = response.json()
     return response_json['access_token']
 
-def check_access_token(token):
-    url = 'https://www.googleapis.com/oauth2/v3/tokeninfo'
-    response = requests.get(url, params={'access_token': token})
-
-    if response.status_code == 200:
-        print("Access token is valid.")
-        return response.json()  # Returns details about the token
-    else:
-        print("Access token is invalid or expired.")
-        print("Response:", response.json())
-        return None
 
 # Update MAIL_PASSWORD dynamically with access token
 def update_mail_password():
@@ -66,25 +55,20 @@ def contact():
     form = ContactForm(request.form)
 
     if request.method == 'POST':
-        print("Received POST request.")
+     
 
         # Validate form
         if form.validate() == False:
-            print("Form validation failed.", form.errors)
+           
             flash('All fields are required.')
             return render_template('contact.html', form=form)
         else:
-            print("Form is valid.")
+          
             update_mail_password()  # Get a fresh access token
 
             # Check if the access token is valid
             access_token = app.config.get("MAIL_PASSWORD")  # Assuming MAIL_PASSWORD is set to access token
-            token_info = check_access_token(access_token)
-
-            if not token_info:
-                print("Invalid access token.")
-                flash('Failed to send the message due to an invalid access token.', 'danger')
-                return render_template('contact.html', form=form)
+        
 
             # Prepare and send the email
             msg = Message(form.subject.data, sender=os.getenv("SEND_MAIL"), recipients=[receivemail])
@@ -97,7 +81,7 @@ def contact():
                 mail.send(msg)
                 flash('Your message has been sent successfully!', 'success')
             except Exception as e:
-                print(f"Failed to send email: {e}")
+              
                 flash('Failed to send the message. Try again later.', 'danger')
 
             return render_template('contact.html', success=True)
--- shellScripts/generateReqandDeploy.sh
+++ shellScripts/generateReqandDeploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 pip freeze > requirements.txt
 flyctl deploy --app portfolio-web-app
+echo "Requirements file generated and app deployed to fly.io.\nWOOOOOOOOOOOOOOO"
